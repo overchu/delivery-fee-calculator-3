053a837a266045cbcb87b3367768678e702102b1
Claire Lin <chunhuilin7516@gmail.com>
2023-02-03 19:36:04 +0800

    Create event handlers for each of the input fields

--- src/components/DeliveryForm.js
+++ src/components/DeliveryForm.js
@@ -7,4 +7,22 @@ const DevlieryForm = (props) => {
   const [paramaters, setParamaters] = useState({cart: 0, distance: 0, amount: 0});
   const [deliveryFee, setDeliveryFee] = useState('');
 
-}
+  }
+
+  const timeValueHandler = (event) => {
+    setTime(event.target.value);
+  };
+
+  const handleChange = (event) => {
+    setParamaters(prev => (
+      {
+        ...prev,
+        [event.target.name]: parseInt(event.target.value, 10)
+      })
+    );
+  };
+
+  const deliveryPriceChangeHandler = (event) => {
+    event.preventDefault();
+    setDeliveryFee(deliveryFee);
+  };
