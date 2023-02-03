ce2a5b2fd038b3e21797a45ad4f4c15868f8a25f
Claire Lin <chunhuilin7516@gmail.com>
2023-02-03 19:21:20 +0800

    Define DeliveryForm component and initialize state variable

---
+++ src/components/DeliveryForm.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+import './DeliveryForm.css'
+
+const DevlieryForm = (props) => {
+  const [time, setTime] = useState('');
+  const [paramaters, setParamaters] = useState({cart: 0, distance: 0, amount: 0});
+  const [deliveryFee, setDeliveryFee] = useState('');
+
+}
