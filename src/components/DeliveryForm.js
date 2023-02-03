f45ee8df0792ffb6c5fd6afa4220ca74f2f6c1ff
Claire Lin <chunhuilin7516@gmail.com>
2023-02-03 19:55:06 +0800

    Create calculateDeliveryFee function

--- src/components/DeliveryForm.js
+++ src/components/DeliveryForm.js
@@ -3,8 +3,11 @@ import React, { useState } from 'react';
 import './DeliveryForm.css'
 
 const DevlieryForm = (props) => {
-  const [time, setTime] = useState('');
-  const [paramaters, setParamaters] = useState({cart: 0, distance: 0, amount: 0});
+  const [orderTime, setorderTime] = useState('');
+  const [paramaters, setParamaters] = useState(
+    {cartValue: 0,
+     deliveryDistance: 0,
+     numberOfItems: 0});
   const [deliveryFee, setDeliveryFee] = useState('');
 
   }
@@ -24,5 +27,37 @@ const DevlieryForm = (props) => {
 
   const deliveryPriceChangeHandler = (event) => {
     event.preventDefault();
-    setDeliveryFee(deliveryFee);
+    setDeliveryFee;
   };
+
+  function calculateDeliveryFee(cartValue, deliveryDistance, numberOfItems,
+    orderTime){
+      let deliveryFee = 0;
+
+      if (!(orderTime instanceof Date)) {
+        orderTime = new Date();
+      }
+
+      if (cartValue >= 100) {
+        return deliveryFee;
+      }
+
+      if (cartValue < 10) {
+        deliveryFee += 10 - cartValue;
+      }
+
+      if (deliveryDistance < 1){
+        deliveryFee += 2;
+      } else {
+        deliveryFee += 2 + Math.ceil((deliveryDistance - 1)/ 0.5);
+      }
+
+      if (numberOfItems >= 5){
+        const bulkFee = Math.min(1.2 * (numberOfItems - 4), 12) * 0.5;
+        deliveryFee += bulkFee;
+      }
+      const rushHourMultiplier = (orderTime.getUTChours() >= 15 && orderTime.getUTCHOurs() < 19)
+      deliveryFee *= rushHourMultiplier;
+      deliveryFee = Math.min(deliveryFee, 15);
+      setDeliveryFee(new_delivery_fee)
+    };
