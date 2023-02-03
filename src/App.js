8dac9e12d4c9243a6b016512d1c75d5ed20ac91b
Claire Lin <chunhuilin7516@gmail.com>
2023-02-03 20:20:59 +0800

    Add deliveryForm component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import DeliveryForm from './components/DeliveryForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Delivery Fee Calculator</p>
+        <DeliveryForm></DeliveryForm>
       </header>
     </div>
   );
