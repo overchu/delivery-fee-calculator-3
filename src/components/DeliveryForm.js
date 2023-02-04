24c08c1a1c9d4b2d29a29a52515bc30d8faf5b13
Claire Lin <chunhuilin7516@gmail.com>
2023-02-04 14:15:45 +0800

    First working code, rushHour function not adding yet

--- src/components/DeliveryForm.js
+++ src/components/DeliveryForm.js
@@ -3,39 +3,33 @@ import React, { useState } from 'react';
 import './DeliveryForm.css'
 
 const DeliveryForm = (props) => {
-  const [orderTime, setorderTime] = useState('');
+  const [date, setDate] = useState('1982-03-21');
   const [paramaters, setParamaters] = useState(
     {cartValue: 0,
      deliveryDistance: 0,
      numberOfItems: 0});
-  const [deliveryFee, setDeliveryFee] = useState('');
 
-  const timeValueHandler = (event) => {
-    setorderTime(event.target.value);
-  };
+  const deliveryFee = calculateDeliveryFee(paramaters.cartValue,
+    paramaters.deliveryDistance, paramaters.numberOfItems, date);
+
+  const dateValueHandler = (event) => {
+    setDate(event.target.value);
+  }
 
   const handleChange = (event) => {
     setParamaters(prev => (
       {
         ...prev,
-        [event.target.name]: parseInt(event.target.value, 10)
+        [event.target.name]: parseInt(event.target.value, 10) || 0
+        //handle empty string
       })
     );
   };
 
-  const deliveryPriceChangeHandler = (event) => {
-    const deliveryFee = calculateDeliveryFee
-    event.preventDefault();
-    setDeliveryFee(deliveryFee);
-  };
-
   function calculateDeliveryFee(cartValue, deliveryDistance, numberOfItems,
-    orderTime){
+    orderDateString){
       let deliveryFee = 0;
-
-      if (!(orderTime instanceof Date)) {
-        orderTime = new Date();
-      }
+     // let orderDate = new Date(orderDateString);
 
       if (cartValue >= 100) {
         return deliveryFee;
@@ -55,10 +49,10 @@ const DeliveryForm = (props) => {
         const bulkFee = Math.min(1.2 * (numberOfItems - 4), 12) * 0.5;
         deliveryFee += bulkFee;
       }
-      const rushHourMultiplier = (orderTime.getUTChours() >= 15 && orderTime.getUTCHOurs() < 19)
-      deliveryFee *= rushHourMultiplier;
+      // const rushHourMultiplier = (orderDate.getUTChours() >= 15 && orderDate.getUTCHOurs() < 19)
+      // deliveryFee *= rushHourMultiplier;
       deliveryFee = Math.min(deliveryFee, 15);
-      setDeliveryFee(deliveryFee);
+      return deliveryFee;
   };
 
   return (
@@ -85,10 +79,7 @@ const DeliveryForm = (props) => {
         <div className='delivery-calculator__control'>
           <label className='delivey-calculator__label'>Time</label>
           <input className='delivery-calculator__input' type="date"
-          value={orderTime} onChange={timeValueHandler} />
-        </div>
-        <div className='delivery-calculator__control'>
-          <button onClick={deliveryPriceChangeHandler}>Calculate delivery price</button>
+          value={date} onChange={dateValueHandler} />
         </div>
         <div className='delivery-calculator__control'>
           <p>Delivery price: {deliveryFee}</p>
